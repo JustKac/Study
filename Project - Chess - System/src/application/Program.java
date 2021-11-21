331d262263da8d4bd742b97015bf6591a8c6d956
Caio Henrique <caiohrick@outlook.com>
2021-11-21 15:19:28 -0300

    Implements piece move

--- Project - Chess - System/src/application/Program.java
+++ Project - Chess - System/src/application/Program.java
@@ -1,21 +1,30 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position pos = new Position(3, 5);
-		System.out.println(pos.toString());
-		
-		Board board = new Board(8, 8);
-		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBorard(chessMatch.getPieces());
-
+		
+		while (true) {
+			UI.printBorard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
