31d7ed34fd1bd2fde459c9ae4aacb13e63018f64
Caio Henrique Cordeiro Lins <caio.henrique@segsat.com>
2022-12-21 09:35:25 -0300

    New Study Project - First commit

---
+++ spring+java+docker+aws/rest-whit-spring-boot-and-java/src/test/java/br/com/segsat/restwhitspringbootandjava/RestWhitSpringBootAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.segsat.restwhitspringbootandjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWhitSpringBootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
