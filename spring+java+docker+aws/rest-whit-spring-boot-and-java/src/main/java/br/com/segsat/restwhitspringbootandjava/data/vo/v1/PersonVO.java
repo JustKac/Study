5fe01040fb17634a15dcdd8e105f887e573e1d93
Caio Henrique Cordeiro Lins <caio.henrique@segsat.com>
2022-12-29 14:35:14 -0300

    New Branch - HATEOAS

--- spring+java+docker+aws/rest-whit-spring-boot-and-java/src/main/java/br/com/segsat/restwhitspringbootandjava/data/vo/v1/PersonVO.java
+++ spring+java+docker+aws/rest-whit-spring-boot-and-java/src/main/java/br/com/segsat/restwhitspringbootandjava/data/vo/v1/PersonVO.java
@@ -2,11 +2,13 @@ package br.com.segsat.restwhitspringbootandjava.data.vo.v1;
 
 import java.io.Serializable;
 
-public class PersonVO implements Serializable {
+import org.springframework.hateoas.RepresentationModel;
+
+public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private Long id;
+    private Long key;
     private String firstName;
     private String lastName;
     private String address;
@@ -16,12 +18,12 @@ public class PersonVO implements Serializable {
 
     }
 
-    public Long getId() {
-        return id;
+    public Long getKey() {
+        return key;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setKey(Long id) {
+        this.key = id;
     }
 
     public String getFirstName() {
@@ -60,7 +62,7 @@ public class PersonVO implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((key == null) ? 0 : key.hashCode());
         result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
         result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
         result = prime * result + ((address == null) ? 0 : address.hashCode());
@@ -77,10 +79,10 @@ public class PersonVO implements Serializable {
         if (getClass() != obj.getClass())
             return false;
         PersonVO other = (PersonVO) obj;
-        if (id == null) {
-            if (other.id != null)
+        if (key == null) {
+            if (other.key != null)
                 return false;
-        } else if (!id.equals(other.id))
+        } else if (!key.equals(other.key))
             return false;
         if (firstName == null) {
             if (other.firstName != null)
