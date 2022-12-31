07d4cf1c0c68dc28959e17a851187104471012fe
Caio Henrique Cordeiro Lins <caio.henrique@segsat.com>
2022-12-31 13:43:13 -0300

    testcontainers and rest-assured implementation

---
+++ spring+java+docker+aws/rest-whit-spring-boot-and-java/src/test/java/br/com/segsat/restwhitspringbootandjava/configs/TestConfig.java
@@ -0,0 +1,10 @@
+package br.com.segsat.restwhitspringbootandjava.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+
+    public static final int SERVER_PORT = 8888;
+
+}
