e4ae396bfd92f99f8f92054bce2ac40ab65372b2
Caio Henrique Cordeiro Lins <caio.henrique@segsat.com>
2023-01-03 16:17:40 -0300

    Inittial JWT implementation

---
+++ spring+java+docker+aws/rest-whit-spring-boot-and-java/src/main/java/br/com/segsat/restwhitspringbootandjava/data/vo/v1/security/AccountCredentialsVO.java
@@ -0,0 +1,67 @@
+package br.com.segsat.restwhitspringbootandjava.data.vo.v1.security;
+
+import java.io.Serializable;
+
+public class AccountCredentialsVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String username;
+    private String passowrd;
+
+    public AccountCredentialsVO(String username, String passowrd) {
+        this.username = username;
+        this.passowrd = passowrd;
+    }
+
+    public AccountCredentialsVO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassowrd() {
+        return passowrd;
+    }
+
+    public void setPassowrd(String passowrd) {
+        this.passowrd = passowrd;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((passowrd == null) ? 0 : passowrd.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AccountCredentialsVO other = (AccountCredentialsVO) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (passowrd == null) {
+            if (other.passowrd != null)
+                return false;
+        } else if (!passowrd.equals(other.passowrd))
+            return false;
+        return true;
+    }
+
+}
