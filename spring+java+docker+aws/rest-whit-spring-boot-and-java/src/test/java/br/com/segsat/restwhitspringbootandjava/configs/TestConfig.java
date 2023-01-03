17d5288a76f75f4a46416f78e0d4e4a0578c8ea5
Caio Henrique Cordeiro Lins <caio.henrique@segsat.com>
2023-01-03 12:07:35 -0300

    cors integration tests

--- spring+java+docker+aws/rest-whit-spring-boot-and-java/src/test/java/br/com/segsat/restwhitspringbootandjava/configs/TestConfig.java
+++ spring+java+docker+aws/rest-whit-spring-boot-and-java/src/test/java/br/com/segsat/restwhitspringbootandjava/configs/TestConfig.java
@@ -7,4 +7,13 @@ public class TestConfig {
 
     public static final int SERVER_PORT = 8888;
 
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+
+    public static final String ORIGIN_FROTA = "https://frota.segsat.com";
+
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String CONTENT_TYPE_XML = "application/xml";
+    public static final String CONTENT_TYPE_YML = "application/x-yaml";
+
 }
